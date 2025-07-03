4c789d962fee88388ea19b73c6f98e30e0d184d1
Vladislavus1 <vlydgames@gmail.com>
2025-07-03 15:14:02 +0300

    commit

---
+++ task52.cpp
@@ -0,0 +1,27 @@
+/*
+You are given three integers x, y, and z, representing the positions of three people on a number line:
+
+    x is the position of Person 1.
+    y is the position of Person 2.
+    z is the position of Person 3, who does not move.
+
+Both Person 1 and Person 2 move toward Person 3 at the same speed.
+
+Determine which person reaches Person 3 first:
+
+    Return 1 if Person 1 arrives first.
+    Return 2 if Person 2 arrives first.
+    Return 0 if both arrive at the same time.
+
+Return the result accordingly.
+*/
+
+class Solution {
+public:
+    int findClosest(int x, int y, int z) {
+        int a = x > z ? x - z : z - x, b = y > z ? y - z : z - y;
+        if (a > b) return 2;
+        else if (a < b) return 1;
+        return 0;
+    }
+};
