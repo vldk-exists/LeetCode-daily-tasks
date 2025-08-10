2e5c912e756070c9eb84438fc7b288a5da610266
Vladislavus1 <vlydgames@gmail.com>
2025-08-10 14:28:14 +0300

    commit

---
+++ task94.cpp
@@ -0,0 +1,26 @@
+/*
+Given head which is a reference node to a singly-linked list. The value of each node in the linked list is either 0 or 1. The linked list holds the binary representation of a number.
+
+Return the decimal value of the number in the linked list.
+
+The most significant bit is at the head of the linked list.
+*/
+
+string getValues(ListNode* head) {
+    string values;
+    ListNode* a = head;
+    while(a != nullptr) {
+        values += '0'+a->val;
+        a = a->next;
+    } 
+    return values;
+}
+
+class Solution {
+public:
+    int getDecimalValue(ListNode* head) {
+        string binaryString = getValues(head);
+
+        return stoi(binaryString, nullptr, 2);        
+    }
+};
