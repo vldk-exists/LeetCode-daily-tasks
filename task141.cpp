2fd5ea3276161f292a65e366f48fd10c4475eecc
Vladislavus1 <vlydgames@gmail.com>
2025-09-21 22:51:53 +0300

    commit

---
+++ task141.cpp
@@ -0,0 +1,18 @@
+/*
+You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+
+Letters are case sensitive, so "a" is considered a different type of stone from "A".
+*/
+
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        int result = 0;
+        
+        for (char stone : stones) {
+            if (jewels.find(stone) != string::npos) result++;
+        }
+
+        return result;
+    }
+};
