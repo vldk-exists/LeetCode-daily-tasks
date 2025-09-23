017e056d3ef2c2be233d645558bf2bd12a5713b7
Vladislavus1 <vlydgames@gmail.com>
2025-09-23 13:07:50 +0300

    commit

---
+++ task143.cpp
@@ -0,0 +1,36 @@
+/*
+The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+
+F(0) = 0, F(1) = 1
+F(n) = F(n - 1) + F(n - 2), for n > 1.
+
+Given n, calculate F(n).
+*/
+
+class Solution {
+public:
+    int f(int n) {
+        if (n == 0) return 0;
+        else if (n == 1) return 1;
+
+        int a = 0;
+        int b = 1;
+
+        for (int i = 0; i < n; i++) {
+            int temp = b;
+            b = a+b;
+            a = temp;
+        }
+
+        return a;
+    }
+
+    int fib(int n) {
+        if (n == 0) return 0;
+        else if (n == 1) return 1;
+
+        int result = f(n-1) + f(n-2);
+
+        return result;      
+    }
+};
