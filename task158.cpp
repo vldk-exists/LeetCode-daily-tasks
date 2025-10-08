1ba76f8d0e047d30adbbd5693d79e1e8976b5e77
Vladislavus1 <vlydgames@gmail.com>
2025-10-08 20:51:20 +0300

    commit

---
+++ task158.cpp
@@ -0,0 +1,18 @@
+/*
+Given an array of strings words and a string s, determine if s is an acronym of words.
+
+The string s is considered an acronym of words if it can be formed by concatenating the first character of each string in words in order. For example, "ab" can be formed from ["apple", "banana"], but it can't be formed from ["bear", "aardvark"].
+
+Return true if s is an acronym of words, and false otherwise. 
+*/
+
+class Solution {
+public:
+    bool isAcronym(vector<string>& words, string s) {
+        string b = "";
+
+        for (const string& word: words) b += word[0];
+
+        return b == s;
+    }
+};
