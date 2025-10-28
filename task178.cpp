495b851e473a0862e9d30baf61371924c7c51e61
Vladislavus1 <vlydgames@gmail.com>
2025-10-28 12:18:05 +0200

    commit

---
+++ task178.cpp
@@ -0,0 +1,57 @@
+/*
+You are given an integer array nums.
+
+Start by selecting a starting position curr such that nums[curr] == 0, and choose a movement direction of either left or right.
+
+After that, you repeat the following process:
+
+    If curr is out of the range [0, n - 1], this process ends.
+    If nums[curr] == 0, move in the current direction by incrementing curr if you are moving right, or decrementing curr if you are moving left.
+    Else if nums[curr] > 0:
+        Decrement nums[curr] by 1.
+        Reverse your movement direction (left becomes right and vice versa).
+        Take a step in your new direction.
+
+A selection of the initial position curr and movement direction is considered valid if every element in nums becomes 0 by the end of the process.
+
+Return the number of possible valid selections.
+*/
+
+class Solution {
+public:
+    bool checkSelection(vector<int> nums, int curr, int direction) {
+        while (curr >= 0 && curr < nums.size()) {
+            if (nums[curr] > 0) {
+                nums[curr]--;
+                direction *= -1;
+
+            }
+
+            curr += direction;
+        }
+
+        return accumulate(nums.begin(), nums.end(), 0) == 0;
+    }
+
+    int countValidSelections(vector<int>& nums) {
+
+        vector<int> zeros;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 0) zeros.push_back(i);
+        }
+
+        int count = 0;
+
+        for (int curr: zeros) {
+            // right direction
+            int direction = 1;  
+            if (checkSelection(nums, curr, direction)) count++;
+
+            // left direction
+            direction = -1;
+            if (checkSelection(nums, curr, direction)) count++;
+        }
+
+        return count;
+    }
+};
