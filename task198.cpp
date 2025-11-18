6461b167205bc81b3ef06f76b06afb9b7f67f9f9
Vladislavus1 <vlydgames@gmail.com>
2025-11-18 16:58:21 +0200

    commit

---
+++ task198.cpp
@@ -0,0 +1,27 @@
+/*
+We have two special characters:
+
+    - The first character can be represented by one bit 0.
+    - The second character can be represented by two bits (10 or 11).
+
+Given a binary array bits that ends with 0, return true if the last character must be a one-bit character.
+*/
+
+class Solution {
+public:
+    bool isOneBitCharacter(vector<int>& bits) {
+        bool isOneBit = false;
+        
+        for (int i = 0; i < bits.size();) {
+            if (bits[i] == 1) {
+                isOneBit = false;
+                i += 2;
+            } else {
+                isOneBit = true;
+                i++;
+            }
+        }
+
+        return isOneBit;
+    }
+};
