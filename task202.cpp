e87db7ef1639453b089b65b1643d6ec28138a347
Vladislavus1 <vlydgames@gmail.com>
2025-11-22 12:53:40 +0200

    commit

---
+++ task202.cpp
@@ -0,0 +1,17 @@
+/*
+You are given an integer array nums. In one operation, you can add or subtract 1 from any element of nums.
+
+Return the minimum number of operations to make all elements of nums divisible by 3.
+*/
+
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        int count = 0;
+        for (const int& i: nums) {
+            if (i % 3 != 0) count++;
+        }
+
+        return count;
+    }
+};
