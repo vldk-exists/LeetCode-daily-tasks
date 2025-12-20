c997dfa3ed86d4a44cbf96e156b39ef13f973f9b
Vladislavus1 <vlydgames@gmail.com>
2025-12-20 17:29:55 +0200

    commit

---
+++ task230.cpp
@@ -0,0 +1,34 @@
+/*
+You are given an array of n strings strs, all of the same length.
+
+The strings can be arranged such that there is one on each line, making a grid.
+
+    - For example, strs = ["abc", "bce", "cae"] can be arranged as follows:
+	abc
+	bce
+	cae
+
+You want to delete the columns that are not sorted lexicographically. In the above example (0-indexed), columns 0 ('a', 'b', 'c') and 2 ('c', 'e', 'e') are sorted, while column 1 ('b', 'c', 'a') is not, so you would delete column 1.
+
+Return the number of columns that you will delete.
+*/
+
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        const int n = strs.size();
+        const int m = strs[0].size();
+        int count = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n - 1; j++) {
+                if (strs[j][i] > strs[j+1][i]) {
+                    count++;
+                    break;
+                }
+            }
+        }
+
+        return count;
+    }
+};
