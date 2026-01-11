e47601db064f5e615a41f40e95bd4d4843fc761a
Vladislavus1 <vlydgames@gmail.com>
2026-01-11 15:41:40 +0200

    commit

---
+++ task252.sql
@@ -0,0 +1,11 @@
+-- Why not? :D
+
+-- Write a solution to find the ids of products that are both low fat and recyclable.
+
+-- Return the result table in any order.
+
+-- The result format is in the following example.
+
+SELECT product_id
+FROM Products
+WHERE low_fats = 'Y' AND recyclable = 'Y'; 
