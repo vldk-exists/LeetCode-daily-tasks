800e60f6885a24d104a4bfda6012f2b072a9a2d2
Vladislavus1 <vlydgames@gmail.com>
2026-02-24 15:17:43 +0200

    commit

---
+++ task296.cpp
@@ -0,0 +1,22 @@
+/*
+Given an array arr of integers, check if there exist two indices i and j such that :
+
+    - i != j
+    - 0 <= i, j < arr.length
+    - arr[i] == 2 * arr[j]
+
+*/
+
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        for (int i = 0; i < arr.size(); i++) {
+            for (int j = 0; j < arr.size(); j++) {
+                if (i != j) {
+                    if (arr[i] == 2*arr[j]) return true;
+                }
+            }
+        }
+        return false;
+    }
+};
