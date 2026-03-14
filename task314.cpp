e2bc32e078bdcc8f951f36b164c2e3037f90e2c9
Vladislavus1 <vlydgames@gmail.com>
2026-03-14 15:49:03 +0200

    commit

---
+++ task314.cpp
@@ -0,0 +1,26 @@
+/*
+You own a Goal Parser that can interpret a string command. The command consists of an alphabet of "G", "()" and/or "(al)" in some order. The Goal Parser will interpret "G" as the string "G", "()" as the string "o", and "(al)" as the string "al". The interpreted strings are then concatenated in the original order.
+
+Given the string command, return the Goal Parser's interpretation of command.
+*/
+
+class Solution {
+public:
+    string interpret(string command) {
+        string result;
+
+        for (int i = 0; i < command.length(); ++i) {
+            if (command[i] == '(') {
+                if (command[i+1] == 'a') {
+                    result += "al";
+                    i += 3;
+                } else {
+                    result += 'o';
+                    i += 1;
+                }
+            } else if (command[i] == 'G') result += 'G';
+        }
+
+        return result;
+    }
+};
