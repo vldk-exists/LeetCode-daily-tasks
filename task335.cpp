d327d344d5163454c716320423bc7be6618e883e
Vladislavus1 <vlydgames@gmail.com>
2026-04-04 12:11:57 +0300

    commit

---
+++ task335.cpp
@@ -0,0 +1,24 @@
+/*
+You are given a 0-indexed array of strings details. Each element of details provides information about a given passenger compressed into a string of length 15. The system is such that:
+
+    - The first ten characters consist of the phone number of passengers.
+    - The next character denotes the gender of the person.
+    - The following two characters are used to indicate the age of the person.
+    - The last two characters determine the seat allotted to that person.
+
+Return the number of passengers who are strictly more than 60 years old.
+*/
+
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int count = 0;
+
+        for (const string& i: details) {
+            int age = (i[11]-'0')*10 + (i[12]-'0');
+            if (age > 60) ++count;
+        }
+
+        return count;
+    }
+};
