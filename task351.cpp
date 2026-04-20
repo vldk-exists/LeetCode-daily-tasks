7bdbcb1ff332b1c4b347e3e87cb9c3eb5776436c
Vladislavus1 <vlydgames@gmail.com>
2026-04-20 15:33:10 +0300

    commit

---
+++ task351.cpp
@@ -0,0 +1,24 @@
+/*
+There are n houses evenly lined up on the street, and each house is beautifully painted. You are given a 0-indexed integer array colors of length n, where colors[i] represents the color of the ith house.
+
+Return the maximum distance between two houses with different colors.
+
+The distance between the ith and jth houses is abs(i - j), where abs(x) is the absolute value of x.
+*/
+
+class Solution {
+public:
+    int maxDistance(vector<int>& colors) {
+        int maxDistance = 0;
+
+        for (int i = 0; i < colors.size(); ++i) {
+            for (int j = 0; j < colors.size(); ++j) {
+                if (colors[i] != colors[j]) {
+                    maxDistance = max(maxDistance, abs(i-j));
+                }
+            }
+        }
+
+        return maxDistance;
+    }
+};
