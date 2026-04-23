fd8defd6e15233aba20dd00fe680f7d5fb6ea61b
Vladislavus1 <vlydgames@gmail.com>
2026-04-23 19:43:59 +0300

    commit

---
+++ task353.cpp
@@ -0,0 +1,25 @@
+/*
+Given two arrays of strings list1 and list2, find the common strings with the least index sum.
+
+A common string is a string that appeared in both list1 and list2.
+
+A common string with the least index sum is a common string such that if it appeared at list1[i] and list2[j] then i + j should be the minimum value among all the other common strings.
+
+Return all the common strings with the least index sum. Return the answer in any order.
+*/
+
+class Solution {
+public:
+    vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
+        unordered_map<string, int> indices;
+        for (int i = 0; i < list1.size(); ++i) indices[list1[i]] = i;
+
+        map<int, vector<string>> result;
+        for (int j = 0; j < list2.size(); ++j) {
+            if (indices.find(list2[j]) != indices.end()) 
+                result[indices[list2[j]]+j].push_back(list2[j]);
+        }
+
+        return (*result.begin()).second;
+    }
+};
