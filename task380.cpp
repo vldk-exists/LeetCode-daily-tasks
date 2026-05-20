ebfb7ecd411ea8e7571b03b0a44f5d51d8daac2f
Vladislavus1 <vlydgames@gmail.com>
2026-05-20 21:03:31 +0300

    commit

---
+++ task380.cpp
@@ -0,0 +1,25 @@
+/*
+You are given a 0-indexed array of positive integers nums. Find the number of triplets (i, j, k) that meet the following conditions:
+
+    - 0 <= i < j < k < nums.length
+    - nums[i], nums[j], and nums[k] are pairwise distinct.
+    	-- In other words, nums[i] != nums[j], nums[i] != nums[k], and nums[j] != nums[k].
+
+Return the number of triplets that meet the conditions.
+*/
+
+class Solution {
+public:
+    int unequalTriplets(vector<int>& nums) {
+        int count = 0;
+        for (int i = 0; i < nums.size()-2; ++i) {
+            for (int j = i+1; j < nums.size()-1; ++j) {
+                for (int k = j+1; k < nums.size(); ++k) {
+                    if (nums[i] != nums[j] && nums[i] != nums[k] && nums[j] != nums[k]) ++count;
+                }
+            }
+        }
+
+        return count;
+    }
+};
