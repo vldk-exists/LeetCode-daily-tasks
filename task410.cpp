27db1107caccbbbb2997bb28a9a99e5b5ce334f5
Vladislavus1 <vlydgames@gmail.com>
2026-06-19 15:27:29 +0300

    commit

---
+++ task410.cpp
@@ -0,0 +1,26 @@
+/*
+You are given a positive integer num consisting of exactly four digits. Split num into two new integers new1 and new2 by using the digits found in num. Leading zeros are allowed in new1 and new2, and all the digits found in num must be used.
+
+    - For example, given num = 2932, you have the following digits: two 2's, one 9 and one 3. Some of the possible pairs [new1, new2] are [22, 93], [23, 92], [223, 9] and [2, 329].
+
+Return the minimum possible sum of new1 and new2.
+*/
+
+class Solution {
+public:
+    int minimumSum(int num) {
+        vector<int> digits;
+
+        while (num > 0) {
+            digits.push_back(num % 10);
+            num /= 10;
+        }
+
+        sort(digits.begin(), digits.end());
+
+        int a = digits[0]*10 + digits[2];
+        int b = digits[1]*10 + digits[3];
+
+        return a+b;
+    }
+};
