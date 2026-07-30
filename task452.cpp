1183f83c1a4c4ab044b92e27c43f85effd52db27
Vladislavus1 <vlydgames@gmail.com>
2026-07-30 10:11:37 +0300

    commit

---
+++ task452.cpp
@@ -0,0 +1,31 @@
+/*
+You are given a string word containing distinct lowercase English letters.
+
+Telephone keypads have keys mapped with distinct collections of lowercase English letters, which can be used to form words by pushing them. For example, the key 2 is mapped with ["a","b","c"], we need to push the key one time to type "a", two times to type "b", and three times to type "c" .
+
+It is allowed to remap the keys numbered 2 to 9 to distinct collections of letters. The keys can be remapped to any amount of letters, but each letter must be mapped to exactly one key. You need to find the minimum number of times the keys will be pushed to type the string word.
+
+Return the minimum number of pushes needed to type word after remapping the keys.
+
+An example mapping of letters to keys on a telephone keypad is given below. Note that 1, *, #, and 0 do not map to any letters.
+*/
+
+class Solution {
+public:
+    int minimumPushes(string word) {
+        int m = 1;
+        int n = 0;
+        int count = 0;
+
+        for (const char& i: word) {
+            if (n == 8) {
+                n = 0;
+                ++m;
+            }
+            ++n;
+            count += m;
+        }
+        
+        return count;
+    }
+};
