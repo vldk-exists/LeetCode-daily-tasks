d38c240b5a2cdf677704132b96924c56939f295d
Vladislavus1 <vlydgames@gmail.com>
2026-08-14 13:29:30 +0300

    commit

---
+++ task466.cpp
@@ -0,0 +1,37 @@
+/*
+You are given the root of a binary tree containing digits from 0 to 9 only.
+
+Each root-to-leaf path in the tree represents a number.
+
+    - For example, the root-to-leaf path 1 -> 2 -> 3 represents the number 123.
+
+Return the total sum of all root-to-leaf numbers. Test cases are generated so that the answer will fit in a 32-bit integer.
+
+A leaf node is a node with no children.
+*/
+
+class Solution {
+public:
+    void func(TreeNode* node, int num, int& sum) {
+        num = num*10 + node->val;
+        if (node->left == nullptr && node->right == nullptr) {
+            sum += num;
+            return;
+        } 
+
+        if (node->left != nullptr) {
+            func(node->left, num, sum);
+        }
+
+        if (node->right != nullptr) {
+            func(node->right, num, sum);
+        }
+    }
+
+    int sumNumbers(TreeNode* root) {
+        int sum = 0;
+        func(root, 0, sum);
+
+        return sum;
+    }
+};
