3bf1dc60b1fcda87a33efe0ab67a8b6ff0586a8b
vldk <113709712+vldk-exists@users.noreply.github.com>
2026-08-16 07:58:39 +0300

    commit

---
+++ task470.cpp
@@ -0,0 +1,22 @@
+/*
+Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it. That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+
+Return the answer in an array.
+*/
+
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        vector<int> res(nums.size(), 0);
+
+        for (int i = 0; i < nums.size(); ++i) {
+            for (int j = 0; j < nums.size(); ++j) {
+                if (i != j) {
+                    if (nums[i]>nums[j]) ++res[i];
+                }
+            }
+        }
+
+        return res;
+    }
+};
