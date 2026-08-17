81a5a7646a195646a7419c8cae3f88e23833f83a
vldk <113709712+vldk-exists@users.noreply.github.com>
2026-08-17 22:32:11 +0300

    commit

---
+++ task471.cpp
@@ -0,0 +1,20 @@
+/*
+Given an integer num, return the number of steps to reduce it to zero.
+
+In one step, if the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
+*/
+
+class Solution {
+public:
+    int numberOfSteps(int num) {
+        int count = 0;
+        while (num > 0) {
+            if (num % 2 == 0)
+                num /= 2;
+            else --num;
+            ++count;
+        }
+
+        return count;
+    }
+};
