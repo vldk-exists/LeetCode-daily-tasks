62361ef5687ac9e4d11acbee90cc9f7726b9b09f
Vladislav <vldk-exists>
2026-08-29 22:26:45 +0300

    commit

---
+++ task482.sql
@@ -0,0 +1,12 @@
+/*
+Write a solution to find all the authors that viewed at least one of their own articles.
+
+Return the result table sorted by id in ascending order.
+
+The result format is in the following example.
+*/
+
+SELECT DISTINCT author_id as id
+FROM Views
+WHERE author_id = viewer_id
+ORDER BY author_id;
