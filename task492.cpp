df80a6a17af1ead4cb7adf28b683cccf3bdf98a3
Vladislav <vldk-exists>
2026-09-08 14:13:10 +0300

    commit

---
+++ task492.cpp
@@ -0,0 +1,20 @@
+/*
+You are given an integer n.
+
+Return the total number of commas used when writing all integers from [1, n] (inclusive) in standard number formatting.
+
+In standard formatting:
+
+    - A comma is inserted after every three digits from the right.
+    - Numbers with fewer than 4 digits contain no commas.
+
+*/
+
+class Solution {
+public:
+    int countCommas(int n) {
+        if (n >= 1000) return n - 999;
+        
+        return 0;
+    }
+};
